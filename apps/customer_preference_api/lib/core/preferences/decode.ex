2778dcf417a5434488a4481a8794f65499b5d072
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-03-03 18:37:01 +0000

    Experiment with decode specific module

--- apps/customer_preference_api/lib/core/preferences/decode.ex
+++ apps/customer_preference_api/lib/core/preferences/decode.ex
@@ -1,12 +1,6 @@
-defmodule Core.Preferences.GetCustomerPreferences do
+defmodule Core.Preferences.Decode do
 
-  def for(customer_id) do
-    ExAws.Dynamo.get_item("customer-preferences", %{id: customer_id})
-    |> ExAws.request
-    |> decode
-  end
-
-  defp decode({:ok, response}) when map_size(response) == 0 do
+  def decode({:ok, response}) when map_size(response) == 0 do
     {:error, :not_found}
   end
   defp decode({:ok, response}) do
