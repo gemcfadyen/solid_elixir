960568379ade44e68169f5f7e6111530bf7e2043
gemcfadyen <gemcfadyen@hotmail.co.uk>
2018-03-08 20:20:27 +0000

    Updating to holiday domain

--- apps/customer_preference_api/lib/core/reporting/trends_report.ex
+++ apps/customer_preference_api/lib/core/reporting/trends_report.ex
@@ -1,10 +1,10 @@
-defmodule Core.Reporting.MarketingReport do
+defmodule Core.Reporting.TrendsReport do
   @behaviour Core.Reporting.Formatter
 
   @impl true
   def format_to_rows(data) do
     formatted_data = data
-    IO.inspect("Format marketing report")
+    IO.inspect("Format trends report")
     #... some data clensing and reformatting logic
     {:ok, formatted_data}
   end
